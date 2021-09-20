9291f262082fcefc953bcb363a12fb49af94be30
Tom Victor <vjtomvictor@icloud.com>
2021-09-20 15:11:18 +0530

    Initial commit

---
+++ FirstApp/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Firstline from the App");
+        System.out.println("Second from the App");
+        System.out.println("Third from the App");
+    }
+}
