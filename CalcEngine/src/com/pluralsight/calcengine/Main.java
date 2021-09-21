7bd4239a204977679b83e58e5e7ac8eba4614d92
Tom Victor <vjtomvictor@icloud.com>
2021-09-21 15:36:14 +0530

    switch statements

--- CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -10,20 +10,25 @@ public class Main {
         double result = 0.0d;
         char opCode = 'd';
 
-        if (opCode == 'a')
-            result = value1 + value2;
-        else if (opCode == 's')
-            result = value1 - value2;
-        else if (opCode =='m')
-            result = value1 * value2;
-        else if (opCode == 'd') {
-            if (value2 != 0)
-                result = value1 / value2;
+        switch (opCode){
+            case  ('a'):
+                result = value1 + value2;
+                break;
+            case ('s'):
+                result = value1 - value2;
+                break;
+            case ('m'):
+                result = value1 * value2;
+                break;
+            case ('d'):
+                result = value2 !=0 ? value1/value2 : 0.0d;
+                break;
+            default:
+                System.out.println("Invalid opCode : " + opCode);
+                result = 0.0d;
+                break;
         }
-        else {
-            System.out.println("Invalid opCode : " + opCode);
-            result = 0.0d;
-        }
+
 
         System.out.println(result);
     }
