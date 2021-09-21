327a5a70c2b68cb8429af546ee7d8bcc7c706c41
Tom Victor <vjtomvictor@icloud.com>
2021-09-21 14:56:56 +0530

    added math operations

---
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -0,0 +1,26 @@
+package com.pluralsight.calcengine;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("CalcEngine");
+        double value1 = 100.0d;
+        double value2 = 50.0d;
+        double result = 0.0d;
+        char opCode = 'd';
+
+        if (opCode == 'a')
+            result = value1 + value2;
+        else if (opCode == 's')
+            result = value1 - value2;
+        else if (opCode =='m')
+            result = value1 * value2;
+        else if (opCode == 'd')
+            result = value1/value2;
+        else
+            result = 0.0d;
+
+        System.out.println(result);
+    }
+}
