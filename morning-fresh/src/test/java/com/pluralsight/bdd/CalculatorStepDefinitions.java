97a29c4ba2a5e87081e4bd500cfceb495e9ae4c3
Tom Victor <vjtomvictor@icloud.com>
2021-09-21 23:03:29 +0530

    implemented StepDefinition

--- morning-fresh/src/test/java/com/pluralsight/bdd/CalculatorStepDefinitions.java
+++ morning-fresh/src/test/java/com/pluralsight/bdd/CalculatorStepDefinitions.java
@@ -1,25 +1,26 @@
 package com.pluralsight.bdd;
 
+import com.pluralsight.bdd.calculator.Calculator;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorStepDefinitions {
+    Calculator calculator;
+    Integer result;
     @Given("I have a calculator")
     public void i_have_a_calculator() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        calculator = new Calculator();
     }
 
     @When("I add {int} and {int}")
     public void i_add_and(Integer int1, Integer int2) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        result = calculator.add(int1,int2);
     }
 
     @Then("I should get {int}")
     public void i_should_get(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        assertThat(result).isEqualTo(int1);
     }
 }
---
+++ morning-fresh/src/test/java/com/pluralsight/bdd/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.pluralsight.bdd.calculator;
+
+public class Calculator {
+    public Integer add(Integer int1, Integer int2) {
+        return int1 + int2;
+    }
+}
